5d3eca3d6e8ab1e7027e20f65be611971fddcd4d
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2022-12-20 14:14:22 +0100

    first commit

---
+++ Basics/javascript/tutorial1/javascript/index.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById("run")
+const green = document.querySelector("#ex")
+
+green.addEventListener("click",() => {
+    green.style.backgroundColor = "red";
+    green.innerHTML ="carry on"
+    green.style.borderRadius = "50%"
+})
+
+document.write("<h1>hello dudeee</h1")
+
+popUpWindow = () => {
+    alert("hello world")
+    console.log("hello world from function");
+}
+
+console.log('Hoyhoy');
+
+alert('doihgmsutrkhgs')
+
+/*let
+const
+var */
+
