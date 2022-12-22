f0df61590c4ba69796a5291c676819038cea56e2
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2022-12-22 10:36:20 +0100

    intro

---
+++ Basics/javascript/intro/javascript/debug.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+var company;  //undifined
+console.log(company);
+company = "intec"
+console.log(company);
+var username = 5
+document.getElementById("username").innerText =  username
+
+var student1 = "Ali", student2 = "Kelly"
+console.log(student1,student2);
+
+// diffence betweeen var and let =
+// let can't be redeclare and var is free
+
+// const can't be undifenid and can not be changed
