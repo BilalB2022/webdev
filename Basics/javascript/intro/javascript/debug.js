c5fdf2e6a87475179ca4f03b36df5ec7167afa99
BilalB2022 <BilalBenhammou@student.intecbrussel.be>
2022-12-25 15:29:15 +0100

    js update

--- Basics/javascript/intro/javascript/debug.js
+++ Basics/javascript/intro/javascript/debug.js
@@ -1,13 +1,14 @@
 console.log("hello world");
-var company;  //undifined
+var company; //undifined
 console.log(company);
-company = "intec"
+company = "intec";
 console.log(company);
-var username = 5
-document.getElementById("username").innerText =  username
+var username = "PARAPLUI";
+document.getElementById("username").innerText = username;
 
-var student1 = "Ali", student2 = "Kelly"
-console.log(student1,student2);
+var student1 = "Ali",
+  student2 = "Kelly";
+console.log(student1, student2);
 
 // diffence betweeen var and let =
 // let can't be redeclare and var is free
