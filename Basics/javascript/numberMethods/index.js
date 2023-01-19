fa551a553ed68477cfa5121363c51061b0936a0e
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-01-19 02:58:41 +0100

    update

--- Basics/javascript/numberMethods/index.js
+++ Basics/javascript/numberMethods/index.js
@@ -4,7 +4,7 @@ var result;
 result = "10" // number
 var x = 5
 result = Number(x)
-result = Number("5") === 5
+result = Number('5') === 5
 
 x=100;
 result = x.toString();
@@ -18,23 +18,24 @@ var ThisIsNotANumberYesOrNo ="intec";
 
 result = isNaN(ThisIsNotANumberYesOrNo);
 
-result = isNaN("5");
+result = isNaN('5');
 
 // function IsThisNotANumber(value1,value2){
-//     console.log(value1,value2)
-// }
-
-// IsThisNotANumber(50,60,70)
-
-// parseInt() // convert string to a real integer
-result = "5" // Number() parseInt()
-result = parseInt("5");
-
-// isInteger // return boolean
-
-result = Number.isInteger(5.5)
+    //     console.log(value1,value2)
+    // }
+    
+    // IsThisNotANumber(50,60,70)
+    
+    // parseInt() // convert string to a real integer
+    result = "5" // Number() parseInt()
+    result = parseInt("5");
+    
+    // isInteger // return boolean
+    
+    result = Number.isInteger(5.5) //function for see if it's an integer or not
+    console.log(result);
 
 // +"55" => shortcut of convert string to number
 
 
-console.log(`Result is ${result} and typeof variable is ${typeof result}`)
+console.log(`result is ${result} and de type is ${typeof result}`);
