f384f8396089d7c24fb84b46262053921e06a18d
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-01-23 16:49:26 +0100

    day 23-01-2023 completed

--- Basics/javascript/logicalOperators/index.js
+++ Basics/javascript/logicalOperators/index.js
@@ -1,19 +1,30 @@
-// Comparison Operators
-var n1 = 5; // number
-var n2 = 6;
-var n3 = 5;
-var n4 = "5" // string
-// Equal to (==) Variable value 
-// console.log(n1==n2) // is n1 equal to n2? incorrect/false
-// console.log(n1==n3);
+// Comparison operators
+var n1 = 5
+var n2 = 6
+var n3 = 5
+var n4 = "5"
+// Equal to (==) Variable value
+/* console.log(n1==n2);
+console.log(n1==n2); */
 
-// Equal to (===) Variable value and variable data type
-// console.log(n1 == n4) // control value only
-// console.log(n1 === n4) // control value and data type
+// Equal to (===) Var value and var typeof
+console.log(n1 == n4); // controle value only
+console.log(n1 === n4); // controle value and data type (typeof)
 
 // Not Equal to operator (!=)
-// console.log(n1!=n2) // true
+console.log(n1!=n2);
 
-// not equal operator with duble equal symbol (!==) // control value control type
-// console.log(n1!==n4)
+// Not Equial operator with dubble equal symbol (!==) // controle value controle type 
+console.log(n1!==n4);
 
+// greater than (>)
+console.log(n2>n1);
+console.log(6>5);
+
+// less than (<)
+console.log(n3<n2);
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const map1 = numbers.map(number => {
+    console.log(number %2 === 0 ? "it is even number" : "odd number"); 
+})
