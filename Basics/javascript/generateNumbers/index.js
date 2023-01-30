3f12b568ebabe5e1b8e084bf5e6cfc6ffb9c2884
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-01-30 15:35:23 +0100

    update

---
+++ Basics/javascript/generateNumbers/index.js
@@ -0,0 +1,10 @@
+// Generate from 1 to 100
+// print only the numbers that ar odd
+let arr = []
+
+for(let i = 1 ; i <= 100; i++){
+    if(i%2==!0){
+        arr.push(i)
+    }   
+}
+console.log(arr);
