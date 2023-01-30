084a9cc0ae761f3feeae8ba348c2996d3124a976
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-01-30 10:08:41 +0100

    switchstatement

---
+++ Basics/javascript/switchStatement/index.js
@@ -0,0 +1,73 @@
+const Age = 18;
+
+// switch(Age){
+//     case 18:
+//         console.log('You are too young');
+//         break;
+//     case 25:
+//         console.log('You are grown man!');
+//         break;
+//     case 30:
+//         console.log('dude. it is half of the life');
+//         break;
+//     default:
+//         console.log('No match!')
+// }
+
+// switch(Age>17){
+//     case false:
+//         break;
+//         console.log('You are not allowed to enter casino')
+        
+//     case true:
+//         console.log('You are real gambler :D')
+//         break;
+// }
+
+// var userRequest = '';
+
+// const addUser = () => {
+//     console.log('User is added');
+// }
+
+// const removeUser = () => {
+//     console.log('User is removed');
+// }
+
+// switch(userRequest){
+//     case 'remove':
+//         removeUser();
+//         break
+//     case 'add':
+//         addUser()
+//         break 
+//     default:
+//         console.log('Command not found');   
+// }
+// let days =['zondag','maandag','dinsdag','woensdag','donderdag','vrijdag' ,'zaterdag']
+// let getDay = new Date().getDay()
+
+// switch(getDay){
+//     case 0:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 1:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 2:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 3:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 4:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 5:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+//     case 6:
+//         console.log(`Day of week: ${days[getDay]}`);
+//         break;
+// }
+
