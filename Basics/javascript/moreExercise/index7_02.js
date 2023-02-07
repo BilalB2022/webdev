85d0c704869cacd0272463c905a2951f42a3ad19
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-02-07 13:16:27 +0100

    set, add, delete and update user

--- Basics/javascript/moreExercise/index7_02.js
+++ Basics/javascript/moreExercise/index7_02.js
@@ -7,19 +7,61 @@
 fullname,username,email,password
 */
 
-let users = [{id:5}];
+// let users = [];
 
-const AddNewUser = newUser=>{
-    let id = 0;
-    if(users.length === 0){
-        id++
-        console.log(id);
-    }else{
-        id = users[users.length -1].id
-        id++
-        console.log(id);
-    }
-    // insert new object
-}
+// const AddNewUser = newUser=>{
+//     let id = 0;
+//     if(users.length === 0){
+//         id++
+//         newUser.id = id
+//         users.push(newUser)
+//         // console.log(newUser);
+//     }else{
+//         id = users[users.length -1].id
+//         console.log(`position index number ${users.length-1}`);
+//         id++
+//         newUser.id = id
+//         users.push(newUser)
+//         // console.log(id);
+//     }
+//     // insert new object
+// }
 
-AddNewUser()
+// AddNewUser({
+//     fullname:'John Doe',
+//     username:'john',
+//     email:'john@john.com',
+//     password:'john123'
+// })
+// AddNewUser({
+//     fullname:'John Doe 2',
+//     username:'john 2',
+//     email:'john@john.com2',
+//     password:'john1232'
+// })
+
+// const DeleteUserById = id => {
+//     let findUser = users.findIndex(user => {
+//         return user.id === id
+        
+//     })
+//     if(findUser !== -1){
+//         users.splice(findUser,1)
+//     }else{
+//         console.log("user not found");
+//     }
+//     console.log(findUser);
+// }
+
+// const UpdateUserById = (id,prop,newData) => {
+//     const userIndex = users.findIndex(user => user.id === id) 
+//     console.log(users[userIndex]);
+    
+//     if(userIndex !== -1) {
+//         users[userIndex][prop] = newData
+//     }else(`data not found`)
+// }
+
+// UpdateUserById(1,'email','cora@live.be')
+// console.log(users);
+
