071393bb84300e67daee396d16ccecd3a796ccc3
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-02-27 22:14:22 +0100

    keep async-training

--- Basics/javascript/AsyncProgramming/async-Traning.js
+++ Basics/javascript/AsyncProgramming/async-Traning.js
@@ -1,10 +1,18 @@
-/*
-    1. Create an async Generator function that will yield
-       a Promise that resolves after 1 second with a random
-       number between 0-9 (integer and inclusive)
+const fetchPokemon = () => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      //    resolve({ data: { name: "Pickachu", power: 20 } })
+      reject(new Error("Danger , danger !"))
+   },2000)
+   })
+}
 
-    2. Create the generator object by calling the function ^
+console.log("Program starting ...");
 
-    3. Use a for-await-of loop to loop through the generator
-       indefinitely (ctrl-c to force exit the program)
-*/
+const usePokemon = async () => {
+   const data = await fetchPokemon();
+   console.log(data);
+}
+
+usePokemon()
+console.log("Program Completed");
