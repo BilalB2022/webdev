77c18fd7be7f7e2900699cabed0f5de3fe088fa3
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-02-28 12:19:30 +0100

    d

--- Basics/javascript/JS-OBJECT/exta-point-exo.js
+++ Basics/javascript/JS-OBJECT/exta-point-exo.js
@@ -28,33 +28,77 @@ If the balance is less than the amount, the method should return the following s
 If the account is not found, the method should return the following string: "Invalid card number or pin"
 */
 
-let account = [ 
+// let account = [ 
+//         { name: "John", balance: 1000, cardNumber: 123456789, pin: 1234 },
+//         { name: "Mary", balance: 2000, cardNumber: 987654321, pin: 4321 },
+//         { name: "Peter", balance: 3000, cardNumber: 135792468, pin: 2468 },
+//         { name: "Jane", balance: 4000, cardNumber: 864197532, pin: 7531 },
+//         { name: "Mark", balance: 5000, cardNumber: 159357864, pin: 8642 }
+//     ]
+
+// function ATM (BankCard,pin){
+//     this.bankardNum = BankCard;
+//     this.pinNum = pin;
+//     this.account = account
+    
+// }
+// ATM.prototype.withdraw = function(amount){
+//     for(let i = 0 ; i < this.account.length; i++){
+//         if(this.bankardNum === this.account[i].cardNumber && this.pinNum === this.account[i].pin ){
+//             if(this.account[i].balance >= amount){
+//                 this.account[i].balance -= amount
+//                 return console.log(`Done! Do not forget your card. Your balance is ${this.account[i].balance}`)
+//             }else{
+//                 console.log("Insufficient balance");
+//             }
+//         }
+//     }
+// }
+// let atm = new ATM(123456789, 1234);
+// console.log(atm.withdraw(500));
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+function ATM(bankcard, pin) {
+    this.bankcard = bankcard;
+    this.pin = pin;
+    this.accounts = [
         { name: "John", balance: 1000, cardNumber: 123456789, pin: 1234 },
         { name: "Mary", balance: 2000, cardNumber: 987654321, pin: 4321 },
         { name: "Peter", balance: 3000, cardNumber: 135792468, pin: 2468 },
         { name: "Jane", balance: 4000, cardNumber: 864197532, pin: 7531 },
         { name: "Mark", balance: 5000, cardNumber: 159357864, pin: 8642 }
     ]
-
-function ATM (BankCard,pin){
-    this.bankardNum = BankCard;
-    this.pinNum = pin;
-    this.account = account
-    
 }
 
-ATM.prototype.withdraw = function(amount){
-        for(let i = 0 ; i < this.account.length; i++){
-            if(this.bankardNum === this.account[i].cardNumber && this.pinNum === this.account[i].pin ){
-                if(this.account[i].balance >= amount){
-                    this.account[i].balance -= amount
-                    return console.log(`Done! Do not forget your card. Your balance is ${this.account[i].balance}`)
-                }else{
-                    console.log("Insufficient balance");
-                }
+ATM.prototype.withdraw = function (amount) {
+    let account = this.accounts.find(acc => acc.cardNumber === this.bankcard) // find account
+    // find returns single object only
+    console.log(account)
+
+    // when account(cardnumber) exist is
+    if (account !== undefined) {
+        // pin validation here
+        if (account.pin === this.pin) {
+            console.log('pin is correct')
+            if (account.balance >= amount) {
+                // account.balance = account.balance - amount
+                account.balance -= amount
+                // +=
+                return 'Done, Do not forget take your card back!\nNew balance is ' + account.balance
+            }else{
+                return 'Insufficient funds'
             }
+        } else {
+            console.log('invalid pin')
         }
-  }
-let atm = new ATM(123456789, 1234);
-console.log(atm.withdraw(500));
+        // end of pin validation
+    } else {
+        console.log('You are not customer in our bank or invalid card number')
+    }
+
+    //return account
+}
+
+
 
