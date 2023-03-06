503c9d3c71e76e93608781e33183fa587077188c
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-03-06 20:43:56 +0100

    response exo

--- Basics/javascript/HTTPrequest/homeworkExo/homeWork_03-03-2023/app.js
+++ Basics/javascript/HTTPrequest/homeworkExo/homeWork_03-03-2023/app.js
@@ -3,10 +3,14 @@ const QRCodeImage = document.getElementById("qrcode"); // <img> tag
 const shortURLText = document.getElementById("short_url"); // <p> tag
 
 // do not change these variables
-const baseURL = "https://csclub.uwaterloo.ca/~phthakka/1pt/addURL.php?url="; // base URL for 1pt.co
-let QRCode = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="; // base URL for QR Code image
+let baseURL = "https://csclub.uwaterloo.ca/~phthakka/1pt/addURL.php?url="; // base URL for 1pt.co
+//let QRCode = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="; // base URL for QR Code image
 
-async function MakeShortURL(){
+// "https://csclub.uwaterloo.ca/~phthakka/1pt/addURL.php?url=https://www.youtube.com/watch?v=gIOHSAfJ4_s
+
+// short url : https://1pt.co/7sfdz
+
+async function MakeShortURL() {
     /*
     step 1: get the link from the input tag
 
@@ -28,8 +32,37 @@ async function MakeShortURL(){
 
     */
 
+    console.log(LinkNeedToBeShorten.value)
+    console.log(encodeURIComponent(LinkNeedToBeShorten.value))
+
+    const FullLink = baseURL += encodeURIComponent(LinkNeedToBeShorten.value)
+
+    console.log(FullLink)
+    let res = await fetch(FullLink);
+    let data = res.json() // retrived response data
+    // console.log(data) // data(response object) is ready
+
+    data.then(obj => {
+        const ShortObj = {
+            ShortURL: `https://1pt.co/${obj.short}`,
+            QRImage: ''
+        }
+        console.log(ShortObj)
+        shortURLText.href=ShortObj.ShortURL
+        shortURLText.innerText = `Short URL: ${ShortObj.ShortURL}`
+
+        var qrcode = new QRCode("qrcode", {
+            text: ShortObj.ShortURL,
+            width: 128,
+            height: 128,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
+    })
     // Good Luck!
-   let EncodedLink =  encodeURIComponent(LinkNeedToBeShorten.value)
-   console.log(EncodedLink)
 }
 
+
+
+// https://software.hixie.ch/utilities/js/live-dom-viewer/
