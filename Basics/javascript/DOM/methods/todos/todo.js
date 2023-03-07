5e83c962dcca7e9450d0b1d698e63afc746d523e
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-03-07 14:10:34 +0100

    todoApp

---
+++ Basics/javascript/DOM/methods/todos/todo.js
@@ -0,0 +1,25 @@
+const DeleteItem = document.querySelector('.delete-item')
+
+console.log(DeleteItem);
+console.log(DeleteItem.parentElement);
+console.log(DeleteItem.parentElement.parentElement);
+console.log(DeleteItem.parentElement.parentElement.parentElement);
+console.log(DeleteItem.parentElement.parentElement.parentElement.parentElement);
+console.log(DeleteItem.parentElement.parentElement.parentElement.parentElement.parentElement);
+console.log(DeleteItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+console.log(DeleteItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement); // body
+console.log(DeleteItem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement); //nul
+
+//  document.body.remove()
+
+// DeleteItem.remove()
+
+const ListOfTodos = document.querySelector(".list-group")
+// while (ListOfTodos.children.length>0) {                  // For deleting all the li's (the childs)
+//     ListOfTodos.children[0].remove()
+// }
+console.log(ListOfTodos.firstElementChild);
+console.log(ListOfTodos.lastElementChild);
+
+const buttons = document.querySelectorAll("button")
+console.log(buttons);
