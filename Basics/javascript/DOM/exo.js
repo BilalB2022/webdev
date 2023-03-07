22ef0400e66c657554486da5564563319eaa6192
BilalB2022 <Bilal.Benhammou@student.intecbrussel.be>
2023-03-07 10:32:38 +0100

    table exercise

--- Basics/javascript/DOM/exo.js
+++ Basics/javascript/DOM/exo.js
@@ -1,7 +1,6 @@
 /*
 
 <table>
-
     <thead>
         <tr>
             <th>#</th>
@@ -17,14 +16,70 @@
             <td>toor123</td>
         </tr>
     </tbody>
-
 </table>
 
 
 */
 
-<div class="container">
-
-</div>
-
 // in js you need to use createElement() and appendChild()
+
+
+const ParentElement = document.querySelector('.container');
+
+// 1.Table Parent Element
+const table = document.createElement('table');
+
+// Child-Parent Elements of Table
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody')
+
+// BEGIN THEAD
+// Child of Thead
+const TrOfThead = document.createElement('tr');
+
+// Child of tr in thead
+const ThOfTrInThead1 = document.createElement('th')
+// insert text into th
+// ThOfTrInThead1.innerText= "#"
+ThOfTrInThead1.appendChild(document.createTextNode('#'))
+
+const ThOfTrInThead2 = document.createElement('th')
+ThOfTrInThead2.appendChild(document.createTextNode('username'))
+
+const ThOfTrInThead3 = document.createElement('th')
+ThOfTrInThead3.appendChild(document.createTextNode('password'))
+
+// Append TH elements into Tr OF thead
+TrOfThead.appendChild(ThOfTrInThead1)
+TrOfThead.appendChild(ThOfTrInThead2)
+TrOfThead.appendChild(ThOfTrInThead3)
+
+// Append Tr of thead into thead element
+thead.appendChild(TrOfThead)
+
+// END OF THEAD
+
+// TBODY 
+const Tr_Of_Tbody = document.createElement('tr');
+
+const Td_Of_Tr_inTbody1 = document.createElement('td')
+Td_Of_Tr_inTbody1.appendChild(document.createTextNode('1'))
+
+const Td_Of_Tr_inTbody2 = document.createElement('td')
+Td_Of_Tr_inTbody2.appendChild(document.createTextNode('root'))
+
+const Td_Of_Tr_inTbody3 = document.createElement('td')
+Td_Of_Tr_inTbody3.appendChild(document.createTextNode('toor123'))
+
+Tr_Of_Tbody.appendChild(Td_Of_Tr_inTbody1)
+Tr_Of_Tbody.appendChild(Td_Of_Tr_inTbody2)
+Tr_Of_Tbody.appendChild(Td_Of_Tr_inTbody3)
+
+// TR appended into tbody
+tbody.appendChild(Tr_Of_Tbody)
+
+// Append thead into table
+table.appendChild(thead)
+table.appendChild(tbody)
+
+ParentElement.appendChild(table)
